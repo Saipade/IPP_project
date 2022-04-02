72cfbe760e268aa1b348c0ee2dfb30aec445734d
Saipade <81774545+Saipade@users.noreply.github.com>
2022-04-02 12:46:51 +0200

    rm tester

--- tester/test.php
+++ tester/test.php
@@ -3,7 +3,13 @@
     function parseArguments() {
         global $argc, $argv;
         $arguments = array_slice($argv, 1); // cut test.php
+<<<<<<< Updated upstream
 
+=======
+        foreach ($arguments as $argument) {
+            
+        }
+>>>>>>> Stashed changes
 
     }
 
